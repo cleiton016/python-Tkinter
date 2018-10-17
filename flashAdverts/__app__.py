e74de8ad3fad96bce87b66bbe745a29a278eaf55
Cleiton Luiz <39466647+cleiton016@users.noreply.github.com>
2018-10-17 02:32:33 -0300

    Update __app__.py

--- flashAdverts/__app__.py
+++ flashAdverts/__app__.py
@@ -5,5 +5,5 @@ import Cria_anuncio
 root= Tk()
 #Cadastro.Cadastro(root)
 #usuario.Usuario(root)
-Cria_anuncio.Anuncio(root,'050.907.221-61')
+Cria_anuncio.Anuncio(root,'000.000.000-00')
 root.mainloop()
